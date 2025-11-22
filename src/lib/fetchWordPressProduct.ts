4467edbaceb99a2779661eb5cb4c9a107097bbd2
Sergi0let <s.vashkevych@gmail.com>
2025-11-22 18:59:30 +0200

    Update api url

--- src/lib/fetchWordPressProduct.ts
+++ src/lib/fetchWordPressProduct.ts
@@ -1,4 +1,5 @@
-const WORDPRESS_BASE_URL = "https://ua.gopulse.click/wp-json/nt/v1/feed";
+const WORDPRESS_BASE_URL =
+  "https://gopulse-ua-gopulse.ens2yx.easypanel.host/wp-json/nt/v1/feed";
 
 interface WordPressProductParams {
   product_id: string;
